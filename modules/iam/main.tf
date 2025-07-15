d158d5c6d4b499c6c357dfa1e6d343b7422c3144
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-15 19:31:44 +0500

    add IAM role and policy module

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,1 +1,34 @@
+resource "aws_iam_role" "zero_trust_role" {
+  name = "zero-trust-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "zero_trust_policy" {
+  name = "zero-trust-policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "ec2:Describe*",
+        "s3:List*"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.zero_trust_role.name
+  policy_arn = aws_iam_policy.zero_trust_policy.arn
+}
 
