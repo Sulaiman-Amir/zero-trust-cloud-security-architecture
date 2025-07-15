c69812bfb4919678965a4f53b96c5c69f9dbaa48
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-15 19:35:24 +0500

    define KMS key for secure data encryption

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -1,1 +1,11 @@
+resource "aws_kms_key" "zero_trust_kms" {
+  description             = "KMS key for Zero Trust Architecture"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+
+resource "aws_kms_alias" "zero_trust_alias" {
+  name          = "alias/zero-trust-key"
+  target_key_id = aws_kms_key.zero_trust_kms.key_id
+}
 
