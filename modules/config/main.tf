66151f00d1772f526c0952199f6de992fc65c090
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-15 19:47:28 +0500

    setup AWS Config for compliance tracking

--- modules/config/main.tf
+++ modules/config/main.tf
@@ -1,1 +1,19 @@
+resource "aws_config_configuration_recorder" "main" {
+  name     = "config-recorder"
+  role_arn = aws_iam_role.config.arn
+  recording_group {
+    all_supported = true
+    include_global_resource_types = true
+  }
+}
+
+resource "aws_config_delivery_channel" "main" {
+  name           = "config-delivery"
+  s3_bucket_name = aws_s3_bucket.config_bucket.bucket
+}
+
+resource "aws_config_configuration_recorder_status" "main" {
+  name       = aws_config_configuration_recorder.main.name
+  is_enabled = true
+}
 
