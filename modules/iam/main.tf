a0bdfbd4bacee62d472d52fba75429f4ccae931c
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-15 19:51:19 +0500

    add IAM role for AWS Config recorder

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -32,3 +32,22 @@ resource "aws_iam_role_policy_attachment" "attach" {
   policy_arn = aws_iam_policy.zero_trust_policy.arn
 }
 
+resource "aws_iam_role" "config" {
+  name = "aws-config-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "config.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "config" {
+  role       = aws_iam_role.config.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+}
